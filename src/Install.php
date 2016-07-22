75b7ec805c43105502dd094f1b2b19eb9ed58964
Morrice <mauriciowanderleymartins@gmail.com>
2016-07-22 06:59:40 -0300

    BUG | Correção de posicionamento do arquivo de instalação e correção do PATH de envio do arquivo de config

--- src/Install.php
+++ src/Install.php
@@ -11,19 +11,19 @@ class Install {
 
     public static function install_config(){
         
-        $source  = __DIR__ . "/vendor/morrice/arquivo-remessa/src/config/remetente.php" ;
+        $source  = __DIR__ . "/config/remetente.php";
         
-        $dest  = __DIR__ . "/config/remetente.php" ;
+        $dest  = config_path() . "/remetente.php" ;
         
-        copy($source, $dest);
+            copy($source, $dest);
         
     }
     
     public static function update_config(){
         
-        $source  = __DIR__ . "/vendor/morrice/arquivo-remessa/src/config/remetente.php" ;
+        $source  = __DIR__ . "/config/remetente.php";
         
-        $dest  = __DIR__ . "/config/remetente.php" ;
+        $dest  = config_path() . "/remetente.php" ;
         
         if(!file_exists($dest)){
             copy($source, $dest);
